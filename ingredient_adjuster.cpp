84a2ea4a26e95cb97f6698307d7aa4d6a2f21803
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-13 00:23:07 -0400

    Made edits for textual output and compiled the program.

--- ingredient_adjuster.cpp
+++ ingredient_adjuster.cpp
@@ -5,7 +5,8 @@ int main(){
     float butter_amt = 1.0;
     float flour_amt = 2.75;
     float batch_size = 48.0;
-    // I am using a float for the 48 cookies per batch so that I do not run into issues with integer division with user input.
+    // I am using a float for the 48 cookies per batch so that I do not run into issues with integer division from user input.
+    
     int num_cookies;
     std::cout << "Enter the desired number of cookies as an integer: ";
     std::cin >> num_cookies;
@@ -15,9 +16,9 @@ int main(){
     float required_butter = butter_amt * ingredient_ratio;
     float required_flour = flour_amt * ingredient_ratio;
 
-    std::cout << "Sugar: " << required_sugar << std::endl;
-    std::cout << "Butter: " << required_butter << std::endl;
-    std::cout << "Flour: " << required_flour << std::endl;
+    std::cout << "Required cups of sugar: " << required_sugar << std::endl;
+    std::cout << "Required cups of butter: " << required_butter << std::endl;
+    std::cout << "Required cups of flour: " << required_flour << std::endl;
 
     return 0;
 }
