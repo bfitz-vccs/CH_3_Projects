1929230600ed2c2d33b33b4bcfafe663a00b6d74
bfitz-vccs <bf2877@email.vccs.edu>
2025-07-13 22:26:00 -0400

    Made changes to the textual output for better formatting.

--- interest_calc.cpp
+++ interest_calc.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 int main(){
 
     float principal;
     float interest_rate;
-    float times_compounded;
+    int times_compounded;
 
-    std::cout << "Please enter in the principle amount: ";
+    std::cout << "Please enter in the principal amount: ";
     std::cin >> principal;
     std::cout << "Please enter in the interest rate: ";
     std::cin >> interest_rate;
@@ -17,11 +18,11 @@ int main(){
     float savings_amt = principal * std::pow((1 + ((interest_rate / 100) / times_compounded)), times_compounded);
     float interest = savings_amt - principal;
 
-    std::cout << "Interest Rate: " << interest_rate << "%" << std::endl;
-    std::cout << "Times Compounded: " << times_compounded << std::endl;
-    std::cout << "Principal: $" << principal << std::endl;
-    std::cout << "Interest: $" << interest << std::endl;
-    std::cout << "Amount in Savings: $" << savings_amt << std::endl;
+    std::cout << "Interest Rate: \t\t  " << interest_rate << "%" << std::endl;
+    std::cout << "Times Compounded: \t  " << times_compounded << std::endl;
+    std::cout << "Principal: \t\t$ " << std::setprecision(6) << principal << std::endl;
+    std::cout << "Interest: \t\t$ " << std::setprecision(4) << interest << std::endl;
+    std::cout << "Amount in Savings: \t$ " << std::setprecision(6) << savings_amt << std::endl;
     
     return 0;
 }
